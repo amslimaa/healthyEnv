3edb23614dfcd089bd71e329f0320de690b7bc54
Amos Magalhaes <social.amos@gmail.com>
2024-07-22 16:34:37 -0300

    fork

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,12 +1,12 @@
 export default class Constants {
   // Development URL
-  // static baseUrl = '';
+  //static baseUrl = '';
 
   // Production URL
-  static baseUrl = '';
+  static baseUrl = 'http://localhost:5000';
 
   // Development
-  static ghCliendId = '';
+  static ghCliendId = 'a10f3d99afd5d325bbdb';
 
   // Production
   // static ghCliendId = '';
